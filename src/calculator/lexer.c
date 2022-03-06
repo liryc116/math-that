781ff4b8f5589bd66cee142d66294ed4f478ca3f
cyril <cyril.blin@epita.fr>
2022-03-06 19:00:19 +0100

    Corrected Calculator errors

--- src/calculator/lexer.c
+++ src/calculator/lexer.c
@@ -6,11 +6,38 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <err.h>
-#include <ctype.h>
 
 int is_operator(char c)
 {
     return c=='+' || c=='-' || c=='/' || c=='*';
+}
+
+int is_space(char c)
+{
+    return c==' ' || c=='\f' || c=='\n' || c=='\r' || c=='\t' || c=='\v';
+}
+
+int accept_bin_op(struct token *t)
+{
+    return t->kind==Number || t->value==')';
+}
+
+void two_num(struct token *t)
+{
+    if(t->kind == Number)
+        errx(1, "Two numbers following");
+}
+
+void two_bin_op(struct token *t, char c)
+{
+    if(is_mul_div(t) && (c=='*' || c=='/'))
+        errx(1, "Two binary operators following");
+}
+
+void op_sep(struct token *t)
+{
+    if(t->kind==Operator)
+        errx(1, "Operator before ')'");
 }
 
 struct vector *lex(char *str)
@@ -21,13 +48,12 @@ struct vector *lex(char *str)
     int sep_count = 0;
     size_t size = sizeof(struct token);
 
-    for(; isspace(*str); str++);
+    for(; is_space(*str); str++);
     while(*str!='\0')
     {
         if(*str>='0' && *str<='9')
         {
-            if(t.kind == Number)
-                errx(1, "Two numbers following");
+            two_num(&t);
             t.kind=Number;
             t.value = strtod(str, &str);
         }
@@ -38,12 +64,16 @@ struct vector *lex(char *str)
                 if(*str=='(')
                     sep_count++;
                 else
+                {
+                    op_sep(&t);
                     sep_count--;
+                }
                 t.kind=Separator;
                 t.value = *str;
             }
             else if(is_operator(*str))
             {
+                two_bin_op(&t, *str);
                 t.kind=Operator;
                 t.value = *str;
             }
@@ -52,7 +82,7 @@ struct vector *lex(char *str)
             str++;
         }
         vector_push(v, &t, size);
-        for(; isspace(*str); str++);
+        for(; is_space(*str); str++);
     }
 
     if(sep_count!=0)
--- src/calculator/parser.c
+++ src/calculator/parser.c
@@ -13,6 +13,7 @@ void stack_to_vect(struct vector *v, struct list *stack, size_t size)
 {
         struct token *t = list_pop_front(stack);
         vector_push(v, t, size);
+        printf("%c\n", (int)t->value);
         token_free(t);
 }
 
@@ -20,7 +21,7 @@ int unload_stack(struct vector *v, struct list *stack)
 {
     size_t size = sizeof(struct token);
     while(!list_is_empty(stack) &&
-            ((struct token *)stack->next->data)->value!='(')
+            ((struct token *)stack->next->data)->value!=')')
         stack_to_vect(v, stack, size);
     return list_is_empty(stack);
 }
@@ -28,18 +29,11 @@ int unload_stack(struct vector *v, struct list *stack)
 int should_pop(struct token *o1, struct token *o2)
 {
     return o2->kind!=Separator &&
-        (is_mul_div(o2) || (is_add_min(o2) && is_add_min(o1)) );
-}
-
-// True if a binary operator is accepted after this token
-int accept_bin_op(const struct token *t)
-{
-    return t->kind == Number || (t->kind == Separator &&  t->value == ')');
+        (is_mul_div(o2) || is_add_min(o1) );
 }
 
 struct vector *shunting_yard(struct vector *v)
 {
-    int val_bin_op = 0;
     struct vector *res = vector_new();
     struct list *op_stack = list_new();
     size_t size = sizeof(struct token);
@@ -49,26 +43,23 @@ struct vector *shunting_yard(struct vector *v)
         struct token *t = vector_pop(v);
         if(t->kind == Operator)
         {
-            if(!val_bin_op && is_mul_div(t))
-                errx(1, "Two binary operators following");
             while(!list_is_empty(op_stack) &&
                     should_pop(t, op_stack->next->data))
                 stack_to_vect(res, op_stack, size);
             list_push_front(op_stack, t, size);
         }
-        else if(t->kind == Separator && t->value==')')
+        else if(t->kind == Separator && t->value=='(')
             unload_stack(res, op_stack);
         else if(t->kind == Separator)
             list_push_front(op_stack, t, size);
         else // t->kind == Number
             vector_push(res, t, size);
-        val_bin_op = accept_bin_op(t);
         token_free(t);
     }
 
     while(!unload_stack(res, op_stack))
     {
-        if(((struct token *)op_stack->next->data)->value=='(')
+        if(((struct token *)op_stack->next->data)->value==')')
             token_free(list_pop_front(op_stack));
     }
     list_free(op_stack, &token_free);
@@ -94,7 +85,6 @@ struct tree *parser(struct vector *v)
                 t->left = parser(v);
         }
     }
-
 
     token_free(tok);
     return t;
